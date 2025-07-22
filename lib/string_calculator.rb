33dd1eccab683a335048bba18d76a4f99eae62b4
Ansarissab <zahidensari116@gmail.com>
2025-07-22 20:15:44 +0500

    Optimized the code better OOP encapsulation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,30 @@
 class StringCalculator
   def add(input)
     return 0 if input.empty?
-    delimiter = input.start_with?("//") ? input[2] : ","
 
-    numbers = input.split("//#{delimiter}\n").last
-    numbers = numbers.gsub("\n", delimiter) if numbers.include?("\n")
-    numbers = numbers.split(delimiter).map(&:to_i)
+    delimiter, number_string = extract_delimiter_and_numbers(input)
+    numbers_string = normalize_newlines(number_string, delimiter)
+    numbers = numbers_string.split(delimiter).map(&:to_i)
 
-    if numbers.any?(&:negative?)
-      raise ArgumentError, "Negative numbers are not allowed: #{numbers.select(&:negative?).join(", ")}"
+    negatives = numbers.select(&:negative?)
+    raise ArgumentError, "Negative numbers are not allowed: #{negatives.join(", ")}" if negatives.any?
+
+    numbers.sum
+  end
+
+  private
+
+  def extract_delimiter_and_numbers(input)
+    if input.start_with?("//")
+      header, numbers = input.split("\n", 2)
+      delimiter = header[2..]
+      [delimiter, numbers]
     else
-      return numbers.sum
+      [",", input]
     end
+  end
+
+  def normalize_newlines(input, delimiter)
+    input.gsub("\n", delimiter)
   end
 end
